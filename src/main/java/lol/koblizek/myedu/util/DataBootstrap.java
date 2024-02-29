588f3d8aee2ac45a932f74511e25c1cf69ed7385
KoblizekXD <janprokupek@centrum.cz>
2024-02-29 07:45:40 +0100

    Refactored RequestExceptionHandler and done saome work on DataBootstrap

--- src/main/java/lol/koblizek/myedu/util/DataBootstrap.java
+++ src/main/java/lol/koblizek/myedu/util/DataBootstrap.java
@@ -1,14 +1,24 @@
 package lol.koblizek.myedu.util;
 
+import lol.koblizek.myedu.school.SchoolRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class DataBootstrap implements CommandLineRunner {
+
+    private final SchoolRepository schoolRepository;
+
     @Override
     public void run(String... args) throws Exception {
-
+        if (!Arrays.asList(args).contains("--prod")) {
+            log.warn("Loading test data..., use --prod to disable");
+        }
     }
 }
--- src/main/java/lol/koblizek/myedu/util/RequestExceptionHandler.java
+++ src/main/java/lol/koblizek/myedu/util/RequestExceptionHandler.java
@@ -2,14 +2,14 @@ package lol.koblizek.myedu.util;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ExceptionHandler extends ResponseEntityExceptionHandler {
-
-    @org.springframework.web.bind.annotation.ExceptionHandler({ RuntimeException.class })
-    protected ResponseEntity<Object> handleNotFound(Exception ex, WebRequest request) {
+public class RequestExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler({ RuntimeException.class })
+    protected ResponseEntity<String> handleNotFound(Exception ex, WebRequest request) {
         return ResponseEntity.internalServerError().body("An error occurred: " + ex.getMessage());
     }
 }
